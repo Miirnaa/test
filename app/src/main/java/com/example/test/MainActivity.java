6b734dad69af8e7011b36e7daae3ba01fb56c11b
Mirna Abu Qube <mirna.qube@gmail.com>
2020-03-24 14:01:34 +0200

    commit

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,71 +1,75 @@
 package com.example.test;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.ViewFlipper;
-
-import com.example.test.ui.notifications.NotificationsFragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    private Button button;
-    private Button button2,button4,button5,button6,button7,button8,button9;
-    private Button button3;
-    ViewFlipper v_flipper;
+
+    /*ViewFlipper v_flipper;*/
 ListView listView;
 SearchView searchView;
-ArrayList<String> list;
-ArrayAdapter<String>adapter;
-String[] names={"Fida Pharmacy","Health House Pharmacy","Vanda Phamacy","Aljamea Pharmacy"};
-int [] pics={R.drawable.fida,R.drawable.health,R.drawable.vanda,R.drawable.univer};
+
+
+ArrayList<AdapterView> madapter;
+
+   public class nee extends AppCompatActivity implements SearchView.OnQueryTextListener{
+
+       @Override
+       public boolean onQueryTextSubmit(String query) {
+           return false;
+       }
+
+       @Override
+       public boolean onQueryTextChange(String newText) {
+           return false;
+       }
+   }
     public boolean onCreateOptionsMenu(Menu menu) {
 
       getMenuInflater().inflate(R.menu.menu, menu);
-        MenuItem menuItem = menu.findItem(R.id.searchview);
-    /* SearchView searchView = (SearchView)menuItem.getActionView();
-        searchView.setQueryHint("Type here To Search");
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+     /********fffffffffffffffffff***** MenuItem item = menu.findItem(R.id.searchview);
+        androidx.appcompat.widget.SearchView searchView = (androidx.appcompat.widget.SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new androidx.appcompat.widget.SearchView.OnQueryTextListener() {
+
             @Override
             public boolean onQueryTextSubmit(String query) {
                 return false;
             }
+
             @Override
             public boolean onQueryTextChange(String newText) {
-              adapter.getFilter().filter(newText);
-                return false;
+                newText= newText.toLowerCase();
+                ArrayList<String> newList= new ArrayList<>();
+                for(String sss : s1)
+                {
+                    if (sss.toLowerCase().contains(newText)){
+                        newList.add(sss);
+                    }
+
+                }
+
+                return true;
             }
         });*/
         return true;
@@ -74,9 +78,11 @@ int [] pics={R.drawable.fida,R.drawable.health,R.drawable.vanda,R.drawable.unive
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView listView= findViewById(R.id.lv1);
+
+
+     /*   ListView listView = findViewById(R.id.lv1);
         searchView = (SearchView) findViewById(R.id.searchView);
-        list= new ArrayList<>();
+        list = new ArrayList<>();
         list.add("Medicines");
         list.add("Hair & Cream treatments");
         list.add("Health Care");
@@ -84,8 +90,8 @@ int [] pics={R.drawable.fida,R.drawable.health,R.drawable.vanda,R.drawable.unive
         list.add("Men's Perfume");
         list.add("Mom & Baby");
         listView.setAdapter(adapter);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
-        button = (Button) findViewById(R.id.button1);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);*/
+      /* button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -157,7 +163,7 @@ int [] pics={R.drawable.fida,R.drawable.health,R.drawable.vanda,R.drawable.unive
                 openActivity9();
             }
         });
-
+*/
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
@@ -165,7 +171,7 @@ int [] pics={R.drawable.fida,R.drawable.health,R.drawable.vanda,R.drawable.unive
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-        v_flipper = findViewById(R.id.v_flipper);
+  /*      v_flipper = findViewById(R.id.v_flipper);
 
         int images[] = {R.drawable.pho, R.drawable.images, R.drawable.mer, R.drawable.inv};
 
@@ -173,11 +179,11 @@ int [] pics={R.drawable.fida,R.drawable.health,R.drawable.vanda,R.drawable.unive
         for (int image : images) {
             flipperImages(image);
         }
-    }
+    }*/
 
 
-    public void openActivity2(){
-        Intent intent = new Intent(this, jalapharm.class);
+    /*public void openActivity2(){
+     s   Intent intent = new Intent(this, jalapharm.class);
         startActivity(intent);
     }
     public void openActivity3(){
@@ -205,7 +211,7 @@ int [] pics={R.drawable.fida,R.drawable.health,R.drawable.vanda,R.drawable.unive
         startActivity(intent);
     }
 
-    public void flipperImages(int image) {
+   /* public void flipperImages(int image) {
 
         ImageView imageView = new ImageView(this);
         imageView.setBackgroundResource(image);
@@ -214,8 +220,9 @@ int [] pics={R.drawable.fida,R.drawable.health,R.drawable.vanda,R.drawable.unive
         v_flipper.setAutoStart(true);
         v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
         v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
-    }
+    }*/
 
+    }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
@@ -254,3 +261,4 @@ int [] pics={R.drawable.fida,R.drawable.health,R.drawable.vanda,R.drawable.unive
     }
 
     }
+
